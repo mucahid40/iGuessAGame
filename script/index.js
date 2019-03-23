28b011a43a1ff7cd47b13eb67d5e183b3f273339
Muci <mucahidcanlar@gmail.com>
2019-03-23 19:00:23 +0100

    v0.1

---
+++ script/index.js
@@ -0,0 +1,41 @@
+let hero = {
+    name: "Muci Baba",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "hands",
+        damage: 1,
+    },
+};
+
+
+function rest(whathero) {
+    if (whathero.health === 10) {
+        alert('Health is already full!');
+    }
+    whathero.health= 10;
+    return whathero;
+}
+
+const axe = {
+    type: "axe",
+    damage: 5,
+}
+
+function pickUpItem(whathero, newWeapon) {
+    whathero.inventory.push(newWeapon);
+
+}
+function equipWeapon(newEntry) {
+    if (newEntry.inventory.length === 0) {
+        return;
+    }
+    newEntry.weapon = newEntry.inventory[0];
+
+}
+
+function displayStats() {
+    const main = document.getElementById("main");
+    main.innerText = `Name: ${hero.name} Health: ${hero.health} Weapon: ${hero.weapon.type} Damage${hero.weapon.damage}`;
+}
