eb1d5d9e827c23de5fbf2f6a63b9d1e0e653a699
Muci <mucahidcanlar@gmail.com>
2019-03-23 21:37:40 +0100

    added comments

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+//hero object
 let hero = {
     name: "Muci Baba",
     heroic: true,
@@ -9,7 +10,7 @@ let hero = {
     },
 };
 
-
+//function that gives health to player(not that its needed in the game but still)
 function rest(whathero) {
     if (whathero.health === 10) {
         alert('Health is already full!');
@@ -18,11 +19,13 @@ function rest(whathero) {
     return whathero;
 }
 
+//weapon object
 const axe = {
     type: "axe",
     damage: 5,
 }
 
+//enemy object
 const ghost = {
     type: "enemy",
     health: 10,
@@ -33,47 +36,63 @@ const ghost = {
 }
 
 function pickUpItem(whathero, newWeapon) {
+    //pushing weapon to the front of inventory array
     whathero.inventory.push(newWeapon);
     const thatweapon = document.getElementById("axe");
+    //hiding weapon after picking up to not confuse player
     thatweapon.classList.add("hide");
+    //showing bag image
     const eBag = document.getElementById("bag");
     eBag.classList.remove('hide');
 
 }
 function equipWeapon(newEntry) {
+    //if inventory is empty get out
     if (newEntry.inventory.length === 0) {
         return;
     }
+    // reasigning weapon
     newEntry.weapon = newEntry.inventory[0];
     const eBag = document.getElementById("bag");
     const sGhost = document.getElementById("ghost");
 
+    //hiding bag img to not confuse the player
     eBag.classList.add("hide");
     sGhost.classList.remove("hide")
 
 }
 
+//tried to make it a pop up box but didnt work i could just innerhtml it to main but then the whole game disappears
 function displayStats() {
+    //getting some basic locations here
     const ds = document.getElementById("ds");
     const displaySetting = document.getElementById("displaySetting");
+
+    //if class equals hide remove class and show info in the hidden div
     if (ds.className === 'hide') {
         displaySetting.classList.remove("hide");
         displaySetting.innerText = `Name: ${hero.name} Health: ${hero.health} Weapon: ${hero.weapon.type} Damage${hero.weapon.damage}`;
     } else {
+        //else hide it
         displaySetting.classList.add("hide");
     }
 }
-
+//attack function
 function attack() {
+    //decreasing health when attacked
     ghost.health = ghost.health - hero.weapon.damage;
     hero.health = hero.health - ghost.weapon.damage;
-    
+
+    //tried to add function that flashes when hit
     setTimeout(() => {
         document.getElementById("ghost").style.color = red;
     }, 1000);
 
+    //if ghost is dead disappear
     if (ghost.health === 0) {
         document.getElementById("ghost").classList.add("hide");
+        alert("DAMN he dead")
         alert("Congratulations! you won(i guess)");
     }
+    alert("Ouch")
 }
