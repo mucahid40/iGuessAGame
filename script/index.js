b72b22ff5e53cfebf0fb056380d4c8a8b751dfa4
Muci <mucahidcanlar@gmail.com>
2019-03-23 21:25:48 +0100

    v1.0

--- script/index.js
+++ script/index.js
@@ -5,7 +5,7 @@ let hero = {
     health: 10,
     weapon: {
         type: "hands",
-        damage: 0,
+        damage: 2,
     },
 };
 
@@ -23,8 +23,21 @@ const axe = {
     damage: 5,
 }
 
+const ghost = {
+    type: "enemy",
+    health: 10,
+    weapon: {
+        type: "scare",
+        damage: 2,
+    },
+}
+
 function pickUpItem(whathero, newWeapon) {
     whathero.inventory.push(newWeapon);
+    const thatweapon = document.getElementById("axe");
+    thatweapon.classList.add("hide");
+    const eBag = document.getElementById("bag");
+    eBag.classList.remove('hide');
 
 }
 function equipWeapon(newEntry) {
@@ -32,10 +45,35 @@ function equipWeapon(newEntry) {
         return;
     }
     newEntry.weapon = newEntry.inventory[0];
+    const eBag = document.getElementById("bag");
+    const sGhost = document.getElementById("ghost");
+
+    eBag.classList.add("hide");
+    sGhost.classList.remove("hide")
 
 }
 
 function displayStats() {
-    const main = document.getElementById("main");
-    main.innerText = `Name: ${hero.name} Health: ${hero.health} Weapon: ${hero.weapon.type} Damage${hero.weapon.damage}`;
+    const ds = document.getElementById("ds");
+    const displaySetting = document.getElementById("displaySetting");
+    if (ds.className === 'hide') {
+        displaySetting.classList.remove("hide");
+        displaySetting.innerText = `Name: ${hero.name} Health: ${hero.health} Weapon: ${hero.weapon.type} Damage${hero.weapon.damage}`;
+    } else {
+        displaySetting.classList.add("hide");
+    }
+}
+
+function attack() {
+    ghost.health = ghost.health - hero.weapon.damage;
+    hero.health = hero.health - ghost.weapon.damage;
+    
+    setTimeout(() => {
+        document.getElementById("ghost").style.color = red;
+    }, 1000);
+
+    if (ghost.health === 0) {
+        document.getElementById("ghost").classList.add("hide");
+        alert("Congratulations! you won(i guess)");
+    }
 }
